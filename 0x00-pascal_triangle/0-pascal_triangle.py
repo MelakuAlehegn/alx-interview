76a69ea0c8a5a324ae4025f17b4eb0125e50385d
melakualehegn <melakualehegn34@gmail.com>
2022-12-30 11:46:28 +0300

    Pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+pascals triagle assumes n will be always an integer
+
+Return
+    - a list of lists of integers representing the Pascal’s triangle of n
+    - an empty list if n <= 0
+"""
+
+def pascal_triangle(n):
+    'pascals triangle'
+    final_list = [[1]]
+    currnt_list = []
+    append_list = [1]
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]] 
+    for i in range(n-1):
+        currnt_list.append(1)
+        for i in range(len(append_list) - 1):
+            currnt_list.append(append_list[i] + append_list[i + 1])
+        currnt_list.append(1)
+        final_list.append(currnt_list)
+        append_list = currnt_list
+        currnt_list = []
+    return final_list
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,3 @@
+Returns a list of lists of integers representing the Pascal’s triangle of n
+    Returns an empty list if n <= 0
+    You can assume n will be always an integer
