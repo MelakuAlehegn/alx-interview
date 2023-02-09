74f0431301cc0e4248fe1a4ba7a5ee18055d47fb
melakualehegn <melakualehegn34@gmail.com>
2023-02-10 02:25:04 +0300

    semistandard fixed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,17 @@
 const request = require('request');
 
-const url = "https://swapi-api.alx-tools.com/api/"
+const url = 'https://swapi-api.alx-tools.com/api/';
 request(`${url}films/${process.argv[2]}`, async function (error, response, body) {
-
-    error && console.log(error);
-    const chars = (JSON.parse(response.body).characters);
-    for (const char of chars) {
-        await new Promise((resolve, reject) => {
-            request(char, function (error, response, body) {
-                error && console.log(error)
-                const name = JSON.parse(response.body).name
-                console.log(name)
-                resolve()
-            })
-        })
-
-    }
+  error && console.log(error);
+  const chars = (JSON.parse(response.body).characters);
+  for (const char of chars) {
+    await new Promise((resolve, reject) => {
+      request(char, function (error, response, body) {
+        error && console.log(error);
+        const name = JSON.parse(response.body).name;
+        console.log(name);
+        resolve();
+      });
+    });
+  }
 });
