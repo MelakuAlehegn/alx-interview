b82d2509c383dbb5f86b492f1c68a916fd8dfb88
melakualehegn <melakualehegn34@gmail.com>
2023-02-10 02:27:20 +0300

    requirement fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,18 @@
+#!/usr/bin/node
 const request = require('request');
 
 const url = 'https://swapi-api.alx-tools.com/api/';
-request(`${url}films/${process.argv[2]}`, async function (error, response, body) {
-  error && console.log(error);
-  const chars = (JSON.parse(response.body).characters);
-  for (const char of chars) {
-    await new Promise((resolve, reject) => {
-      request(char, function (error, response, body) {
-        error && console.log(error);
-        const name = JSON.parse(response.body).name;
-        console.log(name);
-        resolve();
-      });
-    });
-  }
+request(`${url}films/${process.argv[1]}`, async function (error, response, body) {
+    error && console.log(error);
+    const chars = (JSON.parse(response.body).characters);
+    for (const char of chars) {
+        await new Promise((resolve, reject) => {
+            request(char, function (error, response, body) {
+                error && console.log(error);
+                const name = JSON.parse(response.body).name;
+                console.log(name);
+                resolve();
+            });
+        });
+    }
 });
